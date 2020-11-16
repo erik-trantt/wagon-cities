3177ec963ce8002d2920de56631965703fcf6ed5
Erik Tran <erik.tran.tt@gmail.com>
2020-11-17 10:44:44 +1100

    refactor: applied Redux state

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -4,11 +4,22 @@ import { connect } from 'react-redux';
 const ActiveCity = (props) => {
   const { city } = props;
 
+  if (!city) {
+    return (
+      <div className="active-city">
+        <h3>Unknown city</h3>
+        <p>Select a city...</p>
+      </div>
+    );
+  }
+
+  const imgUrl = `https://kitt.lewagon.com/placeholder/cities/${city.slug}`;
+
   return (
     <div className="active-city">
       <h3>{city.name}</h3>
-      <div>{city.address}</div>
-      <div><img style={{ width: '100%' }} src={`https://kitt.lewagon.com/placeholder/cities/${city.slug}`} alt="active city" /></div>
+      <p>{city.address}</p>
+      <div><img style={{ width: '100%' }} src={imgUrl} alt="active city" /></div>
     </div>
   );
 };
