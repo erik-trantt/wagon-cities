72206fedeba19a0ad684cd56a67bd1af15d23506
Erik Tran <erik.tran.tt@gmail.com>
2020-11-17 10:43:55 +1100

    refactor: App to function component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import CityList from '../containers/city_list';
 import ActiveCity from '../containers/active_city';
-// import cities from '../data/cities';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <CityList />
-        <ActiveCity />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <CityList />
+      <ActiveCity />
+    </div>
+  );
+};
 
 export default App;
